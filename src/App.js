2ba8c741836518ddd2edb4d7009b098c08c2d534
HaraYoung <qkrtpdud9899@gmail.com>
2022-05-15 23:57:28 +0900

    MB

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './pages/Header';
 import MainEvent from './pages/MainEvent';
 import Main from './pages/Main';
 import BREvent from './pages/BREvent';
-import Search  from './pages/Search';
 
 function App() {
   return (
     <div>
-      <Search/>
+      <Header/>
+      <MainEvent/>
+      <Main/>
+      <BREvent/>
 
     </div>
   );
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,16 +1,29 @@
 import React from "react";
-import styled from "styled-components";
+
 import headerStyles from "./styled/Header.module.scss";
 import HeaderMenu from "./HeaderMenu";
+import SearchForm  from './Search';
+
 import Facebook from "../image/icon/icon_facebook.png";
 import Twitter from "../image/icon/icon_twitter.png";
 import Blog from "../image/icon/icon_blog.png";
 import Instagram from "../image/icon/icon_instgram.png";
 import Youtube from "../image/icon/icon_youtube.png";
 import Logo from "../image/header/logo_baskinrobbins.png";
-import Search from "../image/icon/icon_search.png";
+import SearchImg from "../image/icon/icon_search.png";
+
+
+
 
 const Header = () => {
+  const block={
+    display: 'block'
+  }
+  const none= {
+    display: 'none'
+  }
+  const searchForm= React.useRef();
+
   return (
     <div className={headerStyles.headerArea}>
       <div className={headerStyles.headerWidth}>
@@ -27,7 +40,7 @@ const Header = () => {
         <div className={headerStyles.etc}>
           <span>고객센터</span>
           <span>CONTACTUS</span>
-          <img src={Search} alt="Search" />
+          <img src={SearchImg} alt="Search" />
         </div>
       </div>
       <div className={headerStyles.navTop}>
@@ -36,7 +49,7 @@ const Header = () => {
             <span className={headerStyles.login}></span>
             <span className={headerStyles.join}></span>
           </div>
-          <div className={headerStyles.menuNavArea}>
+          <div className={headerStyles.menuNavArea} onMouseOver={e=>{ }}>
             <span className={headerStyles.flavor}></span>
             <span className={headerStyles.menu}></span>
             <span className={headerStyles.NI}></span>
@@ -46,6 +59,7 @@ const Header = () => {
           </div>
         </div>
       </div>
+      <SearchForm/>
     </div>
   );
 };
